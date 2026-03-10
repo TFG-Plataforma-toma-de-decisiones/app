79e27ed510eec45f440f77aa17d4f0fd08c5d482
AlbertoValenzuelaMunoz1 <albvalmunn@alum.us.es>
2026-03-10 21:00:26 +0100

    Creación barra navegación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,19 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Projects from './projects/Projects';
+import Home from './home/HomePage';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [response,setResponse]=useState("")
-  useEffect(()=>{
-    fetch("http://localhost:8000/api/v1/prueba").then(res=>res.json()).then(res=>setResponse(res.message))
-  },[])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      {response!="" && <p>Backend response:{response}</p>}
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
